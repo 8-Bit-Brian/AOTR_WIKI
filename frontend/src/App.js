20c4630d1c020a3659c2ca621894175a7928d262
8-Bit-Brian <owfl6882@colorado.edu>
2025-08-21 12:25:48 -0600

    unstyled cards working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
+function Unitcard({NAME,COST}) {
+  return(
+    <div className="card">
+      <img src="troop.png" alt="Avatar" style={{width:"100%"}} />
+      <div className="container">
+        <h4><b>{NAME}</b></h4>
+        <p>Cost: {COST}</p>
+      </div>
+    </div> 
+  );
+};
+
 function App() {
-  const [units, setUnits] = useState([]);
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
 
   // Call backend when component loads
   useEffect(() => {
@@ -9,35 +22,44 @@ function App() {
       .then(data => data.json())
       .then(stats => {
         console.log(stats);
-        stats.forEach(unit => {
+        stats.map(unit => {
           console.log(unit.NAME);
         })
       })
       .catch(err => console.error("Fetch error:", err));
   }, []);
 
-  // Run whenever units is changed
-  function SearchForUnit(event) {
-    setUnits(event.target.value);
-    fetch(`http://127.0.0.1:5000/unit-search?unit_name=${event.target.value}`)
-    .then(data => data.json())
-    .then(stats => {
-      stats.forEach(unit => {
-        console.log(unit.NAME);
-      });
-    })
-    .catch(err => console.log(`Failed to find unit! Error: ${err}`))
+  async function SearchForUnit(event) {
+    setQuery(event.target.value);
+
+    const data = await fetch(`http://127.0.0.1:5000/unit-search?unit_name=${event.target.value}`);
+    const stats = await data.json();
+    console.log(stats);
+
+    setResult(stats);
   };
+
   return (
     <div>
       <div>
-        <input type="text" id="uname" name="unit-name" value={units} onChange={SearchForUnit}></input><br></br>
+        <input type="text" id="uname" name="unit-name" value={query} onChange={SearchForUnit}/><br />
       </div>
       <div>
-        <h1>Hello World</h1>
+        <h1>Welcome to the AOTR Unit Wiki!</h1>
         <p>The text box searches for a unit!</p>
       </div>
-  </div>
+
+    
+      <ul>
+        {result.map(unit => (
+          <Unitcard 
+            key={unit.UNIT_ID} 
+            NAME={unit.NAME} 
+            COST={unit.COST} 
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
